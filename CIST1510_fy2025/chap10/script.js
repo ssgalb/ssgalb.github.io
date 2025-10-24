5c6371f80ac91df7d24317e9c7d4056a450c0f4a
ssgalb <ssmith3668@student.gwinnetttech.edu>
2025-10-24 10:42:52 -0400

    chapt10 updates

---
+++ CIST1510_fy2025/chap10/script.js
@@ -0,0 +1,14 @@
+/* Sheldon Smith */
+/* script.js */
+/* 10/24/2025 */
+
+
+/* Hamburger menu function */
+function hamburger () {
+    var menu = document.getElementById("menu-links");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+}
