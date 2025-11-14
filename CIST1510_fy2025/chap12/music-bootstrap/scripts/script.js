3aacc047cab592eec13d3c4e0b6dac49e6a86056
ssgalb <ssmith3668@student.gwinnetttech.edu>
2025-11-13 20:07:09 -0500

    chap12 updates

--- CIST1510_fy2025/chap12/music-bootstrap/scripts/script.js
+++ CIST1510_fy2025/chap12/music-bootstrap/scripts/script.js
@@ -1,8 +1,18 @@
 /*
-    Student Name:
+    Student Name:S. Smith
     File Name: script.js
-    Date: 
+    Date: 11/13/25
 */
+
+/* Hamburger menu function */
+function hamburger () {
+    var menu = document.getElementById("menu-links");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+}
 
 //jQuery for hero image to consume the header window space
 $(document).ready(function(){
